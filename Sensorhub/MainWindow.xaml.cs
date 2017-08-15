ef0253ca03e727d4766ab5f046e2bb1b9ef2ccc8
janschneiderou <jan.schneider@ou.nl>
2017-08-15 12:01:53 +0200

    First Upload

--- Sensorhub/MainWindow.xaml.cs
+++ Sensorhub/MainWindow.xaml.cs
@@ -23,55 +23,276 @@ namespace Sensorhub
     /// </summary>
     public partial class MainWindow : Window
     {
+        //sockets that receive messages from the different applications
         UdpClient receivingUdpClientMyo;
+        UdpClient receivingUdpClientPen;
+        UdpClient receivingUdpClientTest;
+
+        Thread storingThread;
+        bool isStoring = false;
+
+        //Threads that receive messages from the different applications
+        Thread testThread;
+        Thread penThread;
         Thread myoThread;
         bool myoRunning = false;
-        int myoPort = 11001;
+        bool penRunning = false;
+        bool testRunning = false;
+
+        //ports from the different applications
+        int penPort = 11003;
+        int myoPort = 11002;
+        int testPort = 11001;
+
+        //strings for the files of the different applications
+        string penStringFile = "penApp";
+        string testStringFile = "socketTest";
+        string myoStringFile = "MyoApp";
+
+        //strings for the different applications
+        string penString = "";
+        string myoString = "";
+        string testString = "";
+        string penStringTemp = "";
+        string myoStringTemp = "";
+        string testStringTemp = "";
+
+        string storingString = "";
 
         public MainWindow()
         {
             InitializeComponent();
             receivingUdpClientMyo = new UdpClient(myoPort);
+            receivingUdpClientPen = new UdpClient(penPort); ;
+            receivingUdpClientTest = new UdpClient(testPort); ;
         }
 
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-            try
+            
+            startStoring();
+
+            if(myoCheckBox.IsChecked==true)
             {
-                string path = System.IO.Directory.GetCurrentDirectory();
-                System.Diagnostics.Process.Start(path+ "/runables/socketTest.exe");
-                myoRunning = true;
-                myoThread = new Thread(new ThreadStart(myoRunningThread));
-                myoThread.Start();
+                startMyo();
             }
-           catch (Exception xx)
+            if (penCheckBox.IsChecked == true)
             {
-                int x = 1;
-                x++;
+                startPen();
             }
+            if (testCheckBox.IsChecked==true)
+            {
+                startTest();
+            }
+           
+            
         }
 
-      
+        private void startStoring()
+        {
+            storingString = "";
+            isStoring = true;
+            storingThread = new Thread(new ThreadStart(storingRunningThread));
+            storingThread.Start();
+        }
+
+        private void storingRunningThread()
+        {
+            while(isStoring)
+            {
+                if(penString!=penStringTemp|| myoString!=myoStringTemp || testString != testStringTemp)
+                {
+                    double now = DateTime.Now.TimeOfDay.TotalMilliseconds;
+                    storingString = "<" + now + ">" +
+                        "<pen>" + penString + "</pen>" + "<myo>" + myoString + "</myo>" 
+                        + "<test>" + testString + "</test></"+now+">\n";
+                    penStringTemp = penString;
+                    myoStringTemp = myoString;
+                    testStringTemp = testString;
+                }
+                
+            }
+        }
 
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                isStoring = false;
                 myoRunning = false;
-                string path = System.IO.Directory.GetCurrentDirectory();
-                System.Diagnostics.Process[] pp= System.Diagnostics.Process.GetProcesses();
-                System.Diagnostics.Process[] pp1 = System.Diagnostics.Process.GetProcessesByName("socketTest");
-                pp1[0].CloseMainWindow();
+                penRunning = false;
+                testRunning = false;
+                closeTest();
+                closePen();
+                closeMyo();
+                saveString();
+
 
             }
             catch (Exception xx)
             {
-                int x = 1;
-                x++;
+                Console.WriteLine(xx);
             }
         }
 
+        private void saveString()
+        {
+            double now = DateTime.Now.TimeOfDay.TotalMilliseconds;
+            System.IO.File.WriteAllText(now+".txt", storingString);
+            
+        }
 
+
+        #region startApps
+        private void startTest()
+        {
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory();
+                System.Diagnostics.Process.Start(path + "/runables/"+testStringFile+".exe");
+                testRunning = true;
+                testThread = new Thread(new ThreadStart(testRunningThread));
+                testThread.Start();
+            }
+            catch (Exception xx)
+            {
+                Console.WriteLine(xx);
+            }
+        }
+
+        private void startPen()
+        {
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory();
+                System.Diagnostics.Process.Start(path + "/runables/"+penStringFile+".exe");
+                penRunning = true;
+                penThread = new Thread(new ThreadStart(penRunningThread));
+                penThread.Start();
+            }
+            catch (Exception xx)
+            {
+                Console.WriteLine(xx);
+            }
+        }
+
+        private void startMyo()
+        {
+            try
+            {
+                string path = System.IO.Directory.GetCurrentDirectory();
+                System.Diagnostics.Process.Start(path + "/runables/"+myoStringFile+".exe");
+                myoRunning = true;
+                myoThread = new Thread(new ThreadStart(myoRunningThread));
+                myoThread.Start();
+            }
+            catch (Exception xx)
+            {
+                Console.WriteLine(xx);
+            }
+        }
+        #endregion
+
+
+        #region closingApps
+        private void closeMyo()
+        {
+            try
+            {
+                System.Diagnostics.Process[] pp1 = System.Diagnostics.Process.GetProcessesByName(myoStringFile);
+                pp1[0].CloseMainWindow();
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void closePen()
+        {
+            try
+            {
+                System.Diagnostics.Process[] pp1 = System.Diagnostics.Process.GetProcessesByName(penStringFile);
+                pp1[0].CloseMainWindow();
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        private void closeTest()
+        {
+            try
+            {
+                System.Diagnostics.Process[] pp1 = System.Diagnostics.Process.GetProcessesByName(testStringFile);
+                pp1[0].CloseMainWindow();
+            }
+            catch
+            {
+
+            }
+            
+        }
+        #endregion
+
+        #region runningthreads from apps
+
+        private void testRunningThread()
+        {
+            while (testRunning == true)
+            {
+                //Creates an IPEndPoint to record the IP Address and port number of the sender. 
+                // The IPEndPoint will allow you to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+
+                    // Blocks until a message returns on this socket from a remote host.
+                    Byte[] receiveBytes = receivingUdpClientTest.Receive(ref RemoteIpEndPoint);
+
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("This is the message you received " +
+                                                 returnData);
+                    testString = returnData;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        private void penRunningThread()
+        {
+            while (penRunning == true)
+            {
+                //Creates an IPEndPoint to record the IP Address and port number of the sender. 
+                // The IPEndPoint will allow you to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+
+                    // Blocks until a message returns on this socket from a remote host.
+                    Byte[] receiveBytes = receivingUdpClientPen.Receive(ref RemoteIpEndPoint);
+
+                    string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("This is the message you received " +
+                                                 returnData.ToString());
+
+                    penString = returnData.ToString();
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
 
         private void myoRunningThread()
         {
@@ -90,10 +311,7 @@ namespace Sensorhub
 
                     Console.WriteLine("This is the message you received " +
                                                  returnData.ToString());
-                    Console.WriteLine("This message was sent from " +
-                                                RemoteIpEndPoint.Address.ToString() +
-                                                " on their port number " +
-                                                RemoteIpEndPoint.Port.ToString());
+                    myoString = returnData.ToString();
                 }
                 catch (Exception e)
                 {
@@ -101,6 +319,6 @@ namespace Sensorhub
                 }
             }
         }
-
+        #endregion
     }
 }
